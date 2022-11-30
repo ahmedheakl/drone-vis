bf5a9fdd2efc12cc33c27e644492004bcaa8ec72
Ahmed Mohamed Sobhey Heakl <52796111+ahmedheakl@users.noreply.github.com>
2022-11-30 10:34:40 +0200

    setup file (#1)
    * setup file
    
    * main devops struct

---
+++ src/dronevis/__main__.py
@@ -0,0 +1,24 @@
+"""Connect to your drone and run your CV models
+Usage
+------------------
+    
+    $ dronevis [--options]
+Get data about the arguments
+    $ dronevis -help
+Or just run the following to the default
+    
+    $ dronevis
+    
+    
+Version
+------------------
+ - dronevis v0.0.1
+"""
+
+
+def main() -> None:
+    print("Welcome to dronevis")
+    
+    
+if __name__ == "__main__":
+    main()
