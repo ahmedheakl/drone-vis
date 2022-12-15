c81c0b9dc3ba24117a1b60b9e0229ad40d5d99f9
ahmedheakl <ahmed.heakl@ejust.edu.eg>
2022-12-16 00:37:05 +0200

    main docs files added

---
+++ docs/source/conf.py
@@ -0,0 +1,56 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import pathlib
+import sys
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+
+
+project = 'DroneVis'
+copyright = '2022, Ahmed Heakl'
+author = 'Ahmed Heakl'
+release = '0.0.3'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'rst2pdf.pdfbuilder',
+    'sphinxcontrib.napoleon'
+]
+
+pdf_documents = [('index', u'highrldocs', u'Highl Docs', u'Ahmed Heakl'),]
+pdf_stylesheets = ['twocolumn']
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    # 'special-members': '__init__',
+    # 'undoc-members': True,
+    # 'exclude-members': '__weakref__'
+}
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. DroneVis documentation master file, created by
+   sphinx-quickstart on Fri Dec 16 00:31:04 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to DroneVis's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
