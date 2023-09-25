80af33264df76eff0ccdcdc3f34ee0d010fc8dae
Heakl <ahmed.heakl@ejust.edu.eg>
2023-09-25 05:59:02 +0300

    Fixed display errors

--- tests/ui/test_gesture_recognition_thread.py
+++ tests/ui/test_gesture_recognition_thread.py
@@ -1,12 +1,17 @@
 """Test gesture recognition thread"""
-from tkinter.ttk import Label
+import pytest
+import cv2
+import numpy as np
 
 from dronevis.ui.gesture_recognition_thread import GestureThread
 
 
-def test_init():
+def test_init(mocker):
     """Test initailization"""
-    label = Label()
+    label = mocker.Mock()
+
+    label.configure = lambda x: None
+    label.after = lambda *args: None
     thread = GestureThread(label)
     assert thread.model.hands is None
     assert thread.video_index == 0
@@ -14,28 +19,36 @@ def test_init():
     assert thread.running
 
 
-def test_run():
+def test_run(monkeypatch: pytest.MonkeyPatch, mocker):
     """Test run"""
-    label = Label()
+    label = mocker.Mock()
+
+    label.configure = lambda x: None
+    label.after = lambda *args: None
+    mocked = mocker.Mock()
+    imshow_mock = mocker.Mock()
+    mocked.read.return_value = True, np.zeros((100, 100, 3), dtype=np.uint8)
+    monkeypatch.setattr(cv2, "VideoCapture", lambda x: mocked)
+    monkeypatch.setattr(cv2, "imshow", imshow_mock)
+    monkeypatch.setattr(cv2, "waitKey", lambda x: ord("q"))
     thread = GestureThread(label)
     thread.start()
     thread.join()
-    assert not thread.running
+    assert thread.running
 
 
-def test_resume():
-    """Test resume"""
-    label = Label()
-    thread = GestureThread(label)
-    thread.start()
-    thread.resume()
-    thread.join()
-    assert not thread.running
-
-
-def test_stop():
+def test_stop(monkeypatch: pytest.MonkeyPatch, mocker):
     """Test stop"""
-    label = Label()
+    label = mocker.Mock()
+
+    label.configure = lambda x: None
+    label.after = lambda *args: None
+    mocked = mocker.Mock()
+    imshow_mock = mocker.Mock()
+    mocked.read.return_value = True, np.zeros((100, 100, 3), dtype=np.uint8)
+    monkeypatch.setattr(cv2, "VideoCapture", lambda x: mocked)
+    monkeypatch.setattr(cv2, "imshow", imshow_mock)
+    monkeypatch.setattr(cv2, "waitKey", lambda x: ord("q"))
     thread = GestureThread(label)
     thread.start()
     thread.stop()
